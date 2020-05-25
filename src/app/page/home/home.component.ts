31e7635f239b7ef484399f7a2289731f880456ec
Hugo Luis <hugoluis.system@gmail.com>
2020-05-24 23:24:06 -0500

    Mostar un modal con el Video

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from 'src/app/services/youtube.service';
-import { Video } from '../../models/youtube.models'
+import { Video } from '../../models/youtube.models';
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -22,6 +24,26 @@ export class HomeComponent implements OnInit {
           this.videos.push(...resp);
           console.log(this.videos)
         })
+  }
+  mostrarVideo( video: Video){
+
+    console.log(video)
+    Swal.fire({
+      html:`
+      
+      <h4> ${video.title} </h4>
+      <hr>
+      <iframe width="100%" 
+              height="315" 
+              src="https://www.youtube.com/embed/${video.resourceId.videoId}" 
+              frameborder="0" 
+              allow="accelerometer; 
+              autoplay; encrypted-media; 
+              gyroscope; 
+              picture-in-picture" 
+              allowfullscreen>
+      </iframe>`
+    })
   }
 
 }
